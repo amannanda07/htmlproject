5531362da95bb477d27ebac65fe03325fde361b3
amannanda07 <45649098+amannanda07@users.noreply.github.com>
2018-12-16 00:38:43 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+    <style>
+      @import url("/Users/amannanda/Desktop/project2/css/styles.css");
+    </style>
+  </head>
+  <body>
+    <!-- pager is the product i choose -->
+    <!-- heading -->
+    <!-- header -->
+    <header>
+      <h1>
+        Motorola TalkAbout 2Way Pager - Pagewriter / Timeport Functional Powers On
+      </h1>
+
+
+      <ul>
+      <li><a href="/Users/amannanda/Desktop/project2/index.html">Home</a></li>
+      <li><a href="/Users/amannanda/Desktop/project2/about.html">About</a></li>
+      <li><a href="/Users/amannanda/Desktop/project2/info.html">Company Information</a></li>
+      <li><a href="/Users/amannanda/Desktop/project2/promotion.html">Promotion</a></li>
+      <li><a href="/Users/amannanda/Desktop/project2/rate.html">Rating and Review </a></li>
+      <li><a href="/Users/amannanda/Desktop/project2/support.html">Support</a></li>
+      </ul>
+    </header>
+
+
+      <!-- inserted image -->
+      <img src="/Users/amannanda/Desktop/project2/images/pager-elite_med.png" alt="Motorola pager" class="center">
+      <!-- main -->
+      <main class="just">
+      <!-- about pager-->
+      <p>
+        <strong>1921:</strong> The first-pager like system was used by the Detroit Police Department when they successfully put a radio-equipped police car into service.
+      </p>
+
+      <p>
+        <strong>1949:</strong> The very first telephone pager device was patented by Al Gross and was used by New York City’s Jewish Hospital starting in 1950. Even though it wasn’t yet called a pager, the device had already found one of its primary niches: critical communications. Despite Gross’ many innovations (the walkie-talkie and CB radio in addition to the pager, among others), all of his patents expired by the 1970s. But he didn’t hold any resentment that he never profited from his inventions. "They have "permeated our society," he said in 2000, a year before his death, "and I'm delighted."
+      </p>
+
+      <p>
+        <strong>1959:</strong> The term “pager” was coined by Motorola.
+      </p>
+
+      <p>
+        <strong>1960:</strong> John Francis Mitchell combined elements of Motorola's walkie-talkie and automobile radio technologies to create the first transistorized pager.
+      </p>
+
+      <p>
+      <strong>1964:</strong> Motorola began its 40-year reign as the dominant leader in the paging space with the introduction of the first consumer tone-only pager, the Pageboy I. With a tone-only pager, the recipient just received a single tone and knew what action was required. For example, a physician would know he should go directly to the ED or call the hospital operator for further information.
+      </p>
+
+      <p>
+        <strong>1970s:</strong> Tone and voice pagers were invented—after the tone, the pager relayed an audio message. This was a step forward from the tone-only pager because the recipient was immediately given more information, such as “Code Blue in ICU.”
+      </p>
+
+      <p>
+        <strong>1980:</strong> There were about 3.2 million pager users worldwide, but paging still had a limited range (local-area pagers), and were primarily used for critical communications on a specific site, i.e., clinicians within the hospital.
+      </p>
+
+      <p>
+        <strong>1980s:</strong> Wide-area paging was invented, allowing pages to be conveyed over radio waves across a city, state, or even a country. Paging use explodes, becoming popular among consumers for personal use.
+      </p>
+
+      <p>
+        <strong>1980s (early):</strong> Numeric display pagers were introduced and helped keep hospitals quiet because messages weren’t played aloud—instead, the numeric display showed a number on top of the device that served as either the extension to call or an internal code for a predetermined action. You could also initiate pages through a telephone.Motorola Tango
+      </p>
+
+      <p>
+        <strong>1980s (mid):</strong> Alphanumeric display pagers were introduced. They could send a text message through a digital network and initiate in a variety of ways, including operator dispatch, IXO (a device used for sending alphanumeric pages via Telelocator Alphanumeric Protocol (TAP)), and computer.
+      </p>
+
+      <p>
+        <strong>1990s (late):</strong> Two-way pagers, or pagers that included QWERTY keyboards, were introduced so message recipients could respond to the page directly from the device. Many major hospitals and health systems still employ two-way paging today, but one-way pagers remain more popular.
+      </p>
+
+      <p>
+        <strong>1944:</strong> There were 61 million pagers in use, largely because pagers are much more affordable than any other mobile communication method.
+      </p>
+
+      <p>
+        <strong>1995:</strong> Motorola introduced the world’s first two-way pager, the Tango two-way personal messaging pager. It allowed users to receive text messages and e-mail, and reply with a standard response. It also could be connected to a computer to download long messages.
+      </p>
+
+      <p>
+        <strong>RIMInteractivepager1996:</strong> Research In Motion (now known as BlackBerry) introduced the Inter@active Pager, which allowed users to receive and send messages thanks to its full keyboard and graphical display.
+      </p>
+
+      <p>
+        <strong>2001:</strong> Motorola and Glenayre, the dominant paging equipment manufacturers, began to exit the paging space.
+      </p>
+
+      <p>
+        <strong>2004:</strong> USA Mobility was formed from the merger of Arch Wireless, Inc. and Metrocall Holdings, Inc., combining the two leading independent paging and wireless messaging companies in the United States.
+      </p>
+
+      <p>
+        <strong>2007:</strong> USA Mobility acquired full ownership of GTES, which provided rights to the Glenayre IP and software source code associated with the GL3000 and other Glenayre product lines.
+      </p>
+
+      <p>
+        <strong>2011:</strong> USA Mobility acquired Amcom Software, creating a company at the forefront of critical communications.
+      </p>
+
+      <p>
+        <strong>2012:</strong> USA Mobility took action on behalf of customers to ensure service continuity to replace the aging Glenayre products. USA Mobility led development of new software based on the Glenayre source code and introduced off-the-shelf server hardware and Open Systems Linux OS.
+      </p>
+
+      <p>
+        <strong>2014:</strong> USA Mobility and Amcom Software came together under single identity and new, exciting brand: Spok. Spok delivers over 100 million messages each month for its customers.Spok T5 Pager
+      </p>
+
+      <p>
+        <strong>2015:</strong> Spok introduced the T5, an alphanumeric pager with an encrypted paging option. With secure messaging capabilities and display-lock security features, this device provides a powerful tool for healthcare and emergency response communication.
+      </p>
+
+      <p>
+        <strong>2016:</strong> Spok unveils the T52 pager, a two-way pager with encrypted capabilities. With the enabled encryption service from Spok, the T52 is the only two-way pager on the market that supports secure messaging and helps healthcare organizations meet HIPAA compliance requirements.
+      </p>
+
+      <p>
+        <strong>Today,</strong> pagers are embraced by the same groups who used the very first versions: public safety and healthcare professionals. Even with the proliferation of smartphones, pagers remain popular in these industries because of the reliability of the paging networks. No matter how sophisticated smartphones become, until cellular networks can match the survivable architecture of the paging networks, pagers will remain a staple of critical communications for years to come.
+      </p>
+    </main>
+
+    <!-- footer -->
+    <footer>
+      <small>
+        All rights reserved | Aman Nanda
+      </small>
+    </footer>
+  </body>
+</html>
